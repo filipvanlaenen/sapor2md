cd63bf1312f895e8bbb7f503f6b5add50d6a97da
Filip van Laenen <f.a.vanlaenen@ieee.org>
2019-07-19 14:24:57 +0200

    Added a first unit test

--- src/main/java/net/filipvanlaenen/sapor2md/ProbabilityMassFunction.java
+++ src/main/java/net/filipvanlaenen/sapor2md/ProbabilityMassFunction.java
@@ -1,5 +1,31 @@
 package net.filipvanlaenen.sapor2md;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProbabilityMassFunction {
+
+    private final Map<Object, Double> map = new HashMap<Object, Double>();
+
+    ProbabilityMassFunction(Object... objects) {
+        if (objects.length % 2 == 1) {
+            throw new IllegalArgumentException(
+                    "The number of arguments to construct a probability mass function should be even.");
+        }
+        for (int i = 0; i < objects.length; i += 2) {
+            Object key = objects[i];
+            Object value = objects[i + 1];
+            if (value instanceof Number) {
+                map.put(key, (Double) value);
+            } else {
+                throw new IllegalArgumentException(
+                        "The even arguments to construct a probability mass function should be numbers.");
+            }
+        }
+    }
+
+    double getProbability(Object key) {
+        return map.get(key);
+    }
 
 }
--- src/test/java/net/filipvanlaenen/sapor2md/ProbabilityMassFunctionTest.java
+++ src/test/java/net/filipvanlaenen/sapor2md/ProbabilityMassFunctionTest.java
@@ -1,5 +1,44 @@
 package net.filipvanlaenen.sapor2md;
 
-public class ProbabilityMassFunctionTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests on <code>ProbabilityMassFunction</code>.
+ */
+public class ProbabilityMassFunctionTest {
+    /**
+     * Test verifying that the constructor throws an
+     * <code>IllegalArgumentException</code> if the number of arguments is odd.
+     */
+    @Test
+    void constructorShouldThrowIllegalArgumentExceptionIfNoOfArgumentsIsOdd() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new ProbabilityMassFunction(0);
+        });
+    }
+
+    /**
+     * Test verifying that the constructor throws an
+     * <code>IllegalArgumentException</code> if one of the even arguments isn't a
+     * number.
+     */
+    @Test
+    void constructorShouldThrowIllegalArgumentExceptionIfOneOfTheEvenArgumentsIsNotANumber() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new ProbabilityMassFunction(0, "foo");
+        });
+    }
+
+    /**
+     * Test verifying that the constructor puts the arguments correctly in the map,
+     * and the getter can fetch one of the probabilities correctly.
+     */
+    @Test
+    void constructorMapsArgumentsCorrectlyToItsInternalMap() {
+        ProbabilityMassFunction pmf = new ProbabilityMassFunction(0, 0.3, 1, 0.4, 2, 0.3);
+        assertEquals(0.3, pmf.getProbability(0));
+    }
 }
