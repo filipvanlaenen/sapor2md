00f7e8e11ded1b95cd820337bdc0f4407013a64e
Filip van Laenen <f.a.vanlaenen@ieee.org>
2020-07-09 08:20:44 +0200

    Issue #1: Added the command PollFiles

--- src/main/java/net/filipvanlaenen/sapor2md/CommandLineInterface.java
+++ src/main/java/net/filipvanlaenen/sapor2md/CommandLineInterface.java
@@ -3,8 +3,12 @@ package net.filipvanlaenen.sapor2md;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -73,10 +77,41 @@ public final class CommandLineInterface {
              * @return A string with the lower bound of the 95 percent confidence interval,
              *         the median and the adjusted median for each parliamentary group.
              */
+            @Override
             String execute(final String... args) {
                 String probabilityMassFunctionsString = readFileIntoString(args[0]);
                 int parliamentSize = Integer.parseInt(args[1]);
                 return SeatProjection.calculateAdjustedMedians(probabilityMassFunctionsString, parliamentSize);
+            }
+        },
+        /**
+         * Command to list out the poll files from a set of directories.
+         */
+        PollFiles {
+            /**
+             * Lists all the poll files found in a set of directories, either all of them or
+             * the ones that are common to all directories.
+             *
+             * @param args
+             *            The arguments for the command, i.e. the mode (All or Common) and
+             *            the list of directories.
+             * @return A multiline string, with on each line the name of a poll file.
+             */
+            @Override
+            String execute(final String... args) {
+                PollFilesMode mode = PollFilesMode.valueOf(args[0]);
+                String[] directories = Arrays.copyOfRange(args, 1, args.length);
+                List<String> pollFiles = new ArrayList<String>();
+                for (String directory : directories) {
+                    try (Stream<Path> walk = Files.walk(Paths.get(directory))) {
+                        List<String> localPollFiles = walk.filter(Files::isRegularFile).map(x -> x.toString())
+                                .filter(f -> f.endsWith(".poll")).collect(Collectors.toList());
+                        mode.mergePollFiles(pollFiles, localPollFiles);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return String.join("\n", pollFiles);
             }
         };
 
@@ -105,5 +140,53 @@ public final class CommandLineInterface {
             }
             return contentBuilder.toString();
         }
+
+        /**
+         * The modes in which poll files from a set of directories can be merged
+         * together.
+         */
+        enum PollFilesMode {
+            /**
+             * Mode to merge poll files from directories retaining all of them.
+             */
+            All {
+                /**
+                 * Merges two lists of poll files together, keeping all poll files from both
+                 * lists.
+                 */
+                @Override
+                void mergePollFiles(final List<String> pollFiles, final List<String> localPollFiles) {
+                    List<String> newPollFiles = new ArrayList<>(localPollFiles);
+                    newPollFiles.removeAll(pollFiles);
+                    pollFiles.addAll(newPollFiles);
+                }
+            },
+            /**
+             * Mode to merge poll files from directories retaining the common ones only.
+             */
+            Common {
+                /**
+                 * Merges two lists of poll files together, keeping only the poll files that are
+                 * common to both lists.
+                 */
+                @Override
+                void mergePollFiles(final List<String> pollFiles, final List<String> localPollFiles) {
+                    List<String> missingPollFiles = new ArrayList<>(pollFiles);
+                    missingPollFiles.removeAll(localPollFiles);
+                    pollFiles.removeAll(missingPollFiles);
+                }
+            };
+
+            /**
+             * Merges two lists of poll files together.
+             *
+             * @param pollFiles
+             *            The list of poll files collected so far.
+             * @param localPollFiles
+             *            The list of poll files from a new directory.
+             */
+            abstract void mergePollFiles(List<String> pollFiles, List<String> localPollFiles);
+        }
+
     }
 }
