f2466b46ca39bcd0862c5e794c6e5e70f6f499cf
Filip van Laenen <f.a.vanlaenen@ieee.org>
2020-07-13 12:15:42 +0200

    Resolved Checkstyle issues for InMemoryStateSummary

--- src/test/java/net/filipvanlaenen/sapor2md/InMemoryStateSummary.java
+++ src/test/java/net/filipvanlaenen/sapor2md/InMemoryStateSummary.java
@@ -2,15 +2,28 @@ package net.filipvanlaenen.sapor2md;
 
 import java.time.OffsetDateTime;
 
-public class InMemoryStateSummary implements StateSummary {
+/**
+ * Class implementing the <code>StateSummary</code> interface, but in-memory,
+ * such that it can be used for testing purposes.
+ */
+public final class InMemoryStateSummary implements StateSummary {
 
+    /**
+     * Number of simulations run on the poll.
+     */
     private long numberOfSimulations;
     /**
      * The timestamp for the state summary file.
      */
     private OffsetDateTime timestamp;
 
-    void setNumberOfSimulations(long numberOfSimulations) {
+    /**
+     * Sets the number of simulations run on the poll.
+     *
+     * @param numberOfSimulations
+     *            The number of simulations run on the poll.
+     */
+    void setNumberOfSimulations(final long numberOfSimulations) {
         this.numberOfSimulations = numberOfSimulations;
     }
 
