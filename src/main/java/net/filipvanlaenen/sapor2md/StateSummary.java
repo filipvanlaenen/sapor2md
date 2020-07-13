969d206ee80e7ec5f31e7cb89896783ad3164bce
Filip van Laenen <f.a.vanlaenen@ieee.org>
2020-07-13 12:49:10 +0200

    Resolved Checkstyle issues for StateSummary

--- src/main/java/net/filipvanlaenen/sapor2md/StateSummary.java
+++ src/main/java/net/filipvanlaenen/sapor2md/StateSummary.java
@@ -2,10 +2,23 @@ package net.filipvanlaenen.sapor2md;
 
 import java.time.OffsetDateTime;
 
+/**
+ * Interface defining the behavior of the state summary for a poll.
+ */
 public interface StateSummary {
 
+    /**
+     * Returns the number of simulations run on the poll.
+     *
+     * @return The number of simulations run on the poll.
+     */
     long getNumberOfSimulations();
 
+    /**
+     * Returns the timestamp of the state summary file.
+     *
+     * @return The timestamp of the state summary file.
+     */
     OffsetDateTime getTimestamp();
 
 }
