7bc48a0b00046fbdac292f1c6ad6fc36845f1eb5
Filip van Laenen <f.a.vanlaenen@ieee.org>
2020-07-13 13:16:39 +0200

    Resolved Checkstyle issues for ProbabilityRange

--- src/main/java/net/filipvanlaenen/sapor2md/ProbabilityRange.java
+++ src/main/java/net/filipvanlaenen/sapor2md/ProbabilityRange.java
@@ -1,6 +1,9 @@
 package net.filipvanlaenen.sapor2md;
 
-public class ProbabilityRange implements Comparable<ProbabilityRange> {
+/**
+ * Class implementing a probability range.
+ */
+public final class ProbabilityRange implements Comparable<ProbabilityRange> {
 
     /**
      * The lower bound of the probability range.
@@ -11,13 +14,22 @@ public class ProbabilityRange implements Comparable<ProbabilityRange> {
      */
     private final Double upperBound;
 
-    ProbabilityRange(Double lowerBound, Double upperBound) {
+    /**
+     * Constructor taking the lower and the upper bound of the probability range as
+     * its parameters.
+     *
+     * @param lowerBound
+     *            The lower bound of the probability range.
+     * @param upperBound
+     *            The upper bound of the probability range.
+     */
+    ProbabilityRange(final Double lowerBound, final Double upperBound) {
         this.lowerBound = lowerBound;
         this.upperBound = upperBound;
     }
 
     @Override
-    public int compareTo(ProbabilityRange other) {
+    public int compareTo(final ProbabilityRange other) {
         if (other.getLowerBound() > lowerBound) {
             return -1;
         } else if (other.getLowerBound() < lowerBound) {
@@ -27,10 +39,20 @@ public class ProbabilityRange implements Comparable<ProbabilityRange> {
         }
     }
 
+    /**
+     * Returns the lower bound of the probability range.
+     *
+     * @return The lower bound of the probability range.
+     */
     Double getLowerBound() {
         return lowerBound;
     }
 
+    /**
+     * Returns the upper bound of the probability range.
+     *
+     * @return The upper bound of the probability range.
+     */
     Double getUpperBound() {
         return upperBound;
     }
