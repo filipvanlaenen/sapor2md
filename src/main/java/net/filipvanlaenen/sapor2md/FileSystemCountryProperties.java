79a5eede5940cdb4829e656f3c9f4db78db3e8ef
Filip van Laenen <f.a.vanlaenen@ieee.org>
2020-07-16 15:45:12 +0200

    Resolved Checkstyle violations

--- src/main/java/net/filipvanlaenen/sapor2md/FileSystemCountryProperties.java
+++ src/main/java/net/filipvanlaenen/sapor2md/FileSystemCountryProperties.java
@@ -23,14 +23,33 @@ public final class FileSystemCountryProperties extends CountryProperties {
         super(readCountryPropertiesFileIntoMap(directory), readTimestamp(directory));
     }
 
-    private static OffsetDateTime readTimestamp(String directory) {
+    /**
+     * Reads the last modified timestamp for the file from the file system.
+     *
+     * @param directory The path to the Sapor directory.
+     * @return The last modified timestamp for the file on the file system.
+     */
+    private static OffsetDateTime readTimestamp(final String directory) {
         return FileSystemServices.getTimestamp(createFilePath(directory));
     }
 
+    /**
+     * Reads the content of the country properties file from the file system and
+     * returns it as a map.
+     *
+     * @param directory The path to the Sapor directory.
+     * @return A map with the properties.
+     */
     private static Map<String, String> readCountryPropertiesFileIntoMap(final String directory) {
         return FileSystemServices.readFileIntoMap(createFilePath(directory));
     }
 
+    /**
+     * Calculates the path to the country properties file in a Sapor directory.
+     *
+     * @param directory The path to the Sapor directory.
+     * @return The path to the country properties file.
+     */
     static String createFilePath(final String directory) {
         return directory + File.separator + COUNTRY_PROPERTIES_FILE_NAME;
     }
