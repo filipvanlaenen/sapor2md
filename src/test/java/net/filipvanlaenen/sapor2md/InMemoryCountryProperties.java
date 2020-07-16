d1b8895ff913608b4cf77936cba9bb879e42a252
Filip van Laenen <f.a.vanlaenen@ieee.org>
2020-07-16 15:47:46 +0200

    Resolved Checkstyle violations

--- src/test/java/net/filipvanlaenen/sapor2md/InMemoryCountryProperties.java
+++ src/test/java/net/filipvanlaenen/sapor2md/InMemoryCountryProperties.java
@@ -3,8 +3,19 @@ package net.filipvanlaenen.sapor2md;
 import java.time.OffsetDateTime;
 import java.util.Map;
 
+/**
+ * Class implementing the abstract <code>CountryProperties</code> class, but in
+ * memory, such that it can be used for testing purposes.
+ */
 public class InMemoryCountryProperties extends CountryProperties {
-    InMemoryCountryProperties(Map<String, String> map, OffsetDateTime timestamp) {
+    /**
+     * Constructor taking a map with the properties and a timestamp as its
+     * parameters.
+     *
+     * @param map       The map containing the country properties.
+     * @param timestamp The last modified timestamp of the file.
+     */
+    InMemoryCountryProperties(final Map<String, String> map, final OffsetDateTime timestamp) {
         super(map, timestamp);
     }
 }
